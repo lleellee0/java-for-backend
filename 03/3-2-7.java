477aa44e24d92e5c750764970e1759f5eb2661c5
Foorogrammer <lleellee013@gmail.com>
2022-11-12 19:22:24 +0900

    Create 3-2-7.java

---
+++ 03/3-2-7.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str1 = new String("is same?");
+        String str2 = new String("is same?");
+
+        System.out.println(str1 == str2); // true or false?
+    }
+}
