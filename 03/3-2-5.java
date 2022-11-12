46a8ce557e7708309f9ad5610f629cc85af5331d
Foorogrammer <lleellee013@gmail.com>
2022-11-12 19:16:48 +0900

    Create 3-2-5.java

---
+++ 03/3-2-5.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        List list = new ArrayList<Integer>();
+        // <Integer>는 ArrayList에 Integer 타입이 저장될 수 있다는 것을 의미한다.
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list.get(1));
+    }
+}
