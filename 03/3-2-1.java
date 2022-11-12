39cccdda67d7834ff1ca681ce7511addcf7bc395
Foorogrammer <lleellee013@gmail.com>
2022-11-12 16:37:28 +0900

    Add files via upload

---
+++ 03/3-2-1.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 1;
+
+        if (number == 1) {
+            System.out.println("if 블록입니다.");
+        } else if (number == 2) {
+            System.out.println("else if 블록입니다.");
+        } else {
+            System.out.println("else 블록입니다.");
+        }
+    }
+}
---
+++ 03/3-2-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("i = " + array[i]);
+        }
+    }
+}
