78d445f3d3037becbc8bb5b95cf73bd35752c9b6
Foorogrammer <lleellee013@gmail.com>
2022-11-12 19:33:29 +0900

    Create 3-3-3.java

---
+++ 03/3-3-3.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer[] integerArray = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> list = Arrays.asList(integerArray);
+
+        List evenList = new ArrayList<Integer>();
+
+        for (int i = 0; i < list.size(); i++) {
+            Integer number = list.get(i);
+            if (number % 2 == 0) { // 2로 나눴을 때의 나머지가 0이면 2의 배수다.
+                evenList.add(number);
+            }
+        }
+
+        for (int i = 0; i < evenList.size(); i++) {
+            System.out.println(evenList.get(i));
+        }
+    }
+}
