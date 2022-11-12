7996007f409b14020c2ccc1cdd834dbf1465d02f
Foorogrammer <lleellee013@gmail.com>
2022-11-12 19:46:26 +0900

    Update 3-3-3.java

--- 03/3-3-3.java
+++ 03/3-3-3.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Integer[] integerArray = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
