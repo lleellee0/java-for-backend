ae08c7b938ae18806950f12884b6534a5ba9da38
Foorogrammer <lleellee013@gmail.com>
2022-11-12 16:57:11 +0900

    Create 3-2-3.java

---
+++ 03/3-2-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int i = 0;
+
+        while (i < array.length) {
+            System.out.println("i = "+ array[i]);
+            i++;
+        }
+    }
+}
