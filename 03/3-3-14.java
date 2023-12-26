4edd675b1e37153505b48b2e80bdfc6a1a6c8c8b
Foorogrammer <lleellee013@gmail.com>
2023-12-27 01:05:57 +0900

    3-3-14 추가

---
+++ 03/3-3-14.java
@@ -0,0 +1,13 @@
+import java.util.Optional;
+
+public class Main {
+    private static Optional<String> getSomeString() {
+        return Optional.ofNullable("public static void");
+    }
+
+    public static void main(String[] args) {
+        Optional<String> str = getSomeString();
+
+        str.ifPresent((string) -> System.out.println(string.toUpperCase()));}
+    }
+}
