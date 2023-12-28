8ea6dfdd2678403138d349304f170a3cf5f63b46
Foorogrammer <lleellee013@gmail.com>
2023-12-28 10:30:14 +0900

    Update 3-3-14.java

--- 03/3-3-14.java
+++ 03/3-3-14.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Optional<String> str = getSomeString();
 
-        str.ifPresent((string) -> System.out.println(string.toUpperCase()));}
+        str.ifPresent((string) -> System.out.println(string.toUpperCase()));
     }
 }
